526a1406e043d35e4d23711b039e3495ad939fef
devaratha31-hub <devaratha31@gmail.com>
2026-02-16 00:16:06 +0530

    Update log.router.js

--- log.router.js
+++ log.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getActiveUsers, getLogs, post, deleteLogs, postServer, getServer} from "./log.controller.js";
+import {getActiveUsers, getLogs, post, deleteLogs, postServer, getServer, postLevel, getLevel} from "./log.controller.js";
 const logRouter = Router();
 logRouter.post('/log', post)
 logRouter.get('/getLogs', getLogs);
@@ -7,4 +7,7 @@ logRouter.get('/totalUsers', getActiveUsers);
 logRouter.delete('/delete', deleteLogs);
 logRouter.post('/fixServer', postServer);
 logRouter.get('/getServer', getServer);
+logRouter.post('/postLevel', postLevel)
+logRouter.post('/getLevel', getLevel)
+
 export default logRouter;
